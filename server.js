a992e9e29d23ae8b34551df6c341550a14b3bdbd
James P Pollock <kajpp00@tamuk.edu>
2024-03-13 17:00:13 -0500

    more connection tests

--- server.js
+++ server.js
@@ -2,50 +2,45 @@
 
 // const port = process.env.PORT
 
-
-
-
 // app.listen(port, () => {
 //   console.log(`Example app listening at http://localhost:${port}`)
 // })
 
-
-
-
-
-import express from 'express'
-const app = express()
-import { config } from "dotenv";
-config()
-
-
-
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
+import {config} from "dotenv";
+config();
+import express from 'express';
+const app = express();
 const PORT = process.env.PORT || 3000
+const uri = process.env.ATLAS_URI;
 
-const connection = process.env.ATLAS_URI
-
-const client = new MongoClient(connection)
+const client = new MongoClient(uri);
 
 let conn;
 
 try {
-  conn = await client.connect()
-
-  console.log("Mongo db connected...")
-} catch (e) {
+  
+  conn = await client.connect(err => {
+    if(err){ console.error(err); return false;}
+    // connection to mongo is successful, listen for requests
+    app.listen(PORT, () => {
+      console.log("listening for requests");
+    })
+  });
+} catch(e) {
   console.error(e)
 }
 
 let db = conn.db("4dx")
 
+console.log(db);
+
 app.get("/", async (req, res) => {
-  let collection = await db.collection("form-responses");
-  let results = await collection.find({}).toArray();
-  res.send(results).status(200);
-});
+    let item = await client.db("4dx").collection("form-responses")
+    let results = await item.find({}).toArray(0)
 
-
+    return res.send(results).status(200)
+})
 
 
 // #############################################################################
@@ -89,4 +84,4 @@ app.use('*', (req, res) => {
 
 // module.exports = app
 
-app.listen(PORT, () => { console.log(`server running on ${PORT}`) })
+// app.listen(PORT, () => { console.log(`server running on ${PORT}`) })
