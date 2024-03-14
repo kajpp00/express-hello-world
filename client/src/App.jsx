104a0907e32b6c4baff7cec7afcfb4bc41e21247
James P Pollock <kajpp00@tamuk.edu>
2024-03-14 16:23:23 -0500

    server stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,13 @@ import { Form, Route, Link, createBrowserRouter, createRoutesFromElements, Route
 import Home, { loader as homeLoader, action as formAction } from './components/Home'
 // import User, { loader as userLoader, action as userAction } from './components/User'
 // import Create, { action as createAction } from './components/Create'
-import ViewData, {loader as viewLoader} from './components/ViewData'
+// import ViewData, {loader as viewLoader} from './components/ViewData'
 // import { action as deleteAction } from './components/Delete'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Nav />}>
     <Route index element={<Home />} loader={homeLoader} action={formAction}/>
-    <Route path="/view" element={<ViewData/>} loader={viewLoader}/>
+    {/* <Route path="/view" element={<ViewData/>} loader={viewLoader}/> */}
     {/* <Route path="/user/:id" element={<User />} action={userAction} loader={userLoader} /> */}
     {/* <Route path="/create" element={<Create />} action={createAction} /> */}
     {/* <Route path="/user/:id/:index" action={deleteAction} /> */}
@@ -33,7 +33,7 @@ function Nav() {
             <Link to="/">Home</Link>
           </li>
           {/* <li><Link to="/create">Create User</Link></li> */}
-          <li><Link to="/view">View Data</Link></li>
+          {/* <li><Link to="/view">View Data</Link></li> */}
         </ul>
       </nav>
       <Outlet />
